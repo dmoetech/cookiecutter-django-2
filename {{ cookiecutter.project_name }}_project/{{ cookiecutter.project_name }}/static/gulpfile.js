405ef18023e3fbef6889e430b45cac1ea23a564a
dmoetech <github@dmoe.tech>
2020-01-29 19:16:05 +0100

    improved gulp

--- {{ cookiecutter.project_name }}_project/{{ cookiecutter.project_name }}/static/gulpfile.js
+++ {{ cookiecutter.project_name }}_project/{{ cookiecutter.project_name }}/static/gulpfile.js
@@ -1,17 +1,25 @@
 //
-// Variables ===================================
+// Variables ==============================
 //
 
-// Load dependencies
-const autoprefixer = require('gulp-autoprefixer');
-const cleancss = require('gulp-clean-css');
-const gulp = require('gulp');
-const npmdist = require('gulp-npm-dist');
-const sass = require('gulp-sass');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
+//
+// Dependencies ===========================
+//
+const sourcemaps = require('gulp-sourcemaps')
+const cleancss = require('gulp-clean-css')
+const npmdist = require('gulp-npm-dist')
+const postcss = require('gulp-postcss')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+const sass = require('gulp-sass')
+const gulp = require('gulp')
+const del = require('del')
 
-// Define paths
+//
+// Paths ==================================
+//
+
 const paths = {
   base:   {
     base:         {
@@ -27,7 +35,7 @@ const paths = {
   src: {
     base : {
       dir: './src',
-      fiels: '.src/**/*'
+      files: '.src/**/*'
     },
     js: {
       dir:    './src/js',
@@ -36,7 +44,15 @@ const paths = {
     scss: {
       dir:    './src/scss',
       files:  './src/scss/**/*',
-      main:   './src/scss/*.scss'
+      main:   './src/scss/main.scss'
+    },
+    img: {
+      dir: './src/img',
+      files: './src/img/**/*'
+    },
+    fonts: {
+      dir: './src/fonts',
+      files: './src/fonts/**/*'
     }
   },
   dist:   {
@@ -45,7 +61,8 @@ const paths = {
       files:  './dist/**/*'
     },
     libs:   {
-      dir:    './dist/libs'
+      dir:    './dist/libs',
+      files:  './dist/libs/**/*'
     },
     js: {
       dir:    './dist/js',
@@ -57,47 +74,117 @@ const paths = {
     },
     img:    {
       dir:    './dist/img',
-      files:  './dist/img/**/*',
+      files:  './dist/img/**/*'
     },
+    fonts: {
+      dir: './dist/fonts',
+      files: './dist/fonts/**/*'
+    }
   }
-};
-
+}
 
 //
-// Tasks ===================================
+// Tasks ==================================
 //
 
-gulp.task('watch', function() {
-  gulp.watch(paths.src.scss.files, gulp.series('scss'));
-});
+function css(cb) {
+  gulp
+    .src(paths.src.scss.main)
+    .pipe(sourcemaps.init())
+    .pipe(sass({includePaths: [paths.base.node.dir]}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.dist.css.dir))
+  cb()
+}
 
-gulp.task('scss', function() {
-  return gulp
-  .src(paths.src.scss.main)
-  .pipe(sass().on('error', sass.logError))
-  .pipe(autoprefixer())
-  .pipe(cleancss())
-  .pipe(gulp.dest(paths.dist.css.dir));
-});
+function cssBuild(cb) {
+  gulp
+    .src(paths.src.scss.main)
+    .pipe(sass({includePaths: [paths.base.node.dir]}).on('error', sass.logError))
+    .pipe(postcss())
+    .pipe(gulp.dest('./dist/css/'))
+  cb()
+}
 
-gulp.task('js', function() {
-  return gulp
-  .src([
-    paths.src.js.files
-    ])
-  .pipe(concat('theme.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest(paths.dist.js.dir));
-});
+function cssClean(cb) {
+  del(paths.dist.css.files)
+  cb()
+}
 
-gulp.task('copy:libs', function() {
-  return gulp
-  .src(npmdist(), { base: paths.base.node.dir })
-  .pipe(gulp.dest(paths.dist.libs.dir));
-});
+function js(cb) {
+  gulp
+    .src(paths.src.js.files)
+    .pipe(sourcemaps.init())
+    .pipe(babel({presets: ['@babel/env']}))
+    .pipe(concat('main.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.dist.js.dir))
+  cb()
+}
 
-gulp.task('build', gulp.series('copy:libs'));
+function jsBuild(cb) {
+  gulp
+    .src(paths.src.js.files)
+    .pipe(babel({presets: ['@babel/env']}))
+    .pipe(uglify())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(paths.dist.js.dir))
+  cb()
+}
 
-gulp.task('default', gulp.series('scss', 'watch'));
+function jsClean(cb) {
+  del(paths.dist.js.files)
+  cb()
+}
 
-gulp.task('setup', gulp.parallel('copy:libs', 'scss', 'js'))
+function libs(cb) {
+  gulp
+    .src(npmdist(), {base: paths.base.node.dir})
+    .pipe(gulp.dest(paths.dist.libs.dir))
+  cb()
+}
+
+function libsClean(cb) {
+  del(paths.dist.libs.files)
+  cb()
+}
+
+function img(cb) {
+  gulp
+    .src(paths.src.img.files)
+    .pipe(gulp.dest(paths.dist.img.dir))
+  cb()
+}
+
+function imgClean(cb) {
+  del(paths.dist.img.files)
+  cb()
+}
+
+function fonts(cb) {
+  gulp
+    .src(paths.src.fonts.files)
+    .pipe(gulp.dest(paths.dist.fonts.dir))
+  cb()
+}
+
+function fontsClean(cb) {
+  del(paths.dist.fonts.files)
+  cb()
+}
+
+//
+// Exports ================================
+//
+
+exports.default = function() {
+  gulp.parallel(css, js)()
+  gulp.watch(paths.src.scss.files, css)
+  gulp.watch(paths.src.js.files, js)
+}
+
+exports.build = gulp.series(gulp.parallel(cssClean, jsClean, imgClean, fontsClean, libsClean), gulp.parallel(img, fonts, libs, cssBuild, jsBuild))
+
+exports.clean = gulp.parallel(cssClean, jsClean, imgClean, fontsClean, libsClean)
+
+exports.dev = gulp.parallel(libs, img, fonts, css, js)
